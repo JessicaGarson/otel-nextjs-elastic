a613608fcbebc760f842fee4df365473914907c8
Jessica Garson <jess.garson@elastic.co>
2025-01-31 13:40:10 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-        <h1 className="text-4xl font-bold mb-8">Welcome to Next.js with OpenTelemetry</h1>
-        <p>This page is instrumented with OpenTelemetry!</p>
+        <h1 className="text-4xl font-bold mb-8">Next.js with OpenTelemetry!</h1>
+        <p>This page is instrumented with OpenTelemetry and uses Elastic as an observablity backend!</p>
       </div>
     </main>
   );
